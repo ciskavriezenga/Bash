26a531216889ecc5890c82af14a787a1f12101e6
loden <l.rietveld@hotmail.com>
2015-09-20 15:02:13 +0200

    finished encoder script, with user prompts and folder creation

--- encoder.sh
+++ encoder.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+# define a list of uncompressed and compressed file types that are valid in sox
+
+UNCOMP=(wav, aiff, raw)
+COMP=(mp3, ogg, flac)
+
 # define what the separate arguments mean
 
 FILEFROM=$1
@@ -10,17 +15,50 @@ FILETO=$2
 FILEFROM=${FILEFROM//./}
 FILETO=${FILETO//./}
 
-# start the loop for every file in the current directory
+# check if the file extension is valid and if the proper number of arguments was given
 
-for file in ./*
-do
+if [[ $# -ne 2 ]]
+then
+  echo "2 arguments required, you supplied $#. Please supply the original and desired file types"
+  exit
 
-  # create the basename of the file
+elif ! [[ $UNCOMP =~ $FILEFROM ]]
+then
+  echo "${FILEFROM} is not a supported file type, only ${UNCOMP[*]} are supported"
+  exit
 
-  BASENAME=`basename $file .$FILEFROM`
-  if [[ $file == *.$FILEFROM ]]
+elif ! [[ $COMP =~ $FILETO ]]
+then
+  echo "${FILETO} is not a supported file type, only ${COMP[*]} are supported"
+  exit
+
+# start the loop for every file in the current directory if the user agrees
+
+else
+
+  echo "This operation will create a new folder and new files equal to the amount of files of file type $FILEFROM in this folder. Continue? [Y/N]:"
+  read response
+
+  if [[ "${response,,}" == "n" ]]
   then
-    `sox $file ${BASENAME}.${FILETO}`
-    echo "Processing ${file#./}..."
+    echo "Exiting..."
+    exit
+
+  elif [[ "${response,,}" == "y" ]]
+  then
+    `mkdir converted_files`
+    for file in ./*
+    do
+
+      # create the basename of the file and then tell sox to convert from the original file to the basename + new extension
+
+      BASENAME=`basename $file .$FILEFROM`
+      if [[ $file == *.$FILEFROM ]]
+      then
+        echo "Processing ${file#./}..."
+        `sox $file ${BASENAME}.${FILETO}`
+        `mv ./${BASENAME}.${FILETO} ./converted_files/`
+      fi
+    done
   fi
-done
+fi
